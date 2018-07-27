dee1eb776036a45aedfc8cf995566b27bdcfde96
Lynette Okoroike <lcokoroike@gmail.com>
2018-07-26 18:50:43 -0700

    Latest update

--- RhythmWorld_Lynette.js
+++ RhythmWorld_Lynette.js
@@ -1,16 +1,29 @@
-$(".top-favorites").on("click", "button", function() {
-    // get text from button that you just clicked and save it to a variable.
-    var searchWord=$(this).text();
-    var queryURL = 'https://api.giphy.com/v1/gifs/search?q='+ searchWord +'&api_key=9Pqpg72Hn0V2mHELBrXruk5iAK4OLles&limit=20';
-    
+$("button").on("click", function() {
+    event.preventDefault();
+    const ArtistPlaylist = $('#Playlist').val().trim();
+    const queryPlaylistURL = 'https://api.spotify.com/v1/search?q=' + ArtistPlaylist + '&type=playlist';
+
+    if (ArtistPlaylist) {
+      console.log(queryPlaylistURL);
+      }
+
+
     $.ajax({
-        url: queryURL,
-        method: "GET"
-      })
-      .then(function(response) {
-        console.log(response);
-        for (i=0; i<response.data.length; i++) {
-          $("body").append("<img src='" + response.data[i].images.original.url + "' />");
-        }
-      });
-});
+        url: queryPlaylistURL,
+        type: "GET",
+        beforeSend: function (xhr) {
+          xhr.setRequestHeader('Authorization', 'Bearer BQA2ffF9uClMOpeWME3jhiRMbszknk-SAnkt-8d5QRavlt7J7dlmHo8H-ZtB65mKu1E4WKAAC8BISOejp9a1gfAeGS2LFsS3UHdiGVM3jvMLQJU0K9KCbxKYg6qQFSS0LmknQxqWo4W7PzJxxzzEzySPW-0j0e5lUXy2rE00238');
+        },
+        success: function (response) {
+          console.log(response);
+          const playlists = response.playlists.items;
+
+          playlists.forEach(function (playlist) {
+             const a = $('<a>');
+             a.text(playlist.name),
+             a.css("display", "block")
+             $('#playlists-search-results').append(a);
+          });  // end of for each loop
+        } // end of success function
+    });// end of ajax
+  });
